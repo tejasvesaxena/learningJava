0016260d4c234188713620dca595a2ff22b4d8be
Tejasve Saxena <tsaxena@ncsu.edu>
2024-12-21 11:14:20 -0500

    initial commit.

---
+++ binarytree/src/binarytree/BinaryTree.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package binarytree;
+
+/**
+ * 
+ */
+public class BinaryTree {
+
+	private IntTreeNode root;
+
+	public void add(int value) {
+
+		root = add(root, value);
+
+	}
+
+	private IntTreeNode add(IntTreeNode root, int value) {
+		if (root == null) {
+			root = new IntTreeNode(value);
+		} else if (root.data > value) {
+			root.left = add(root.left, value);
+		} else if (root.data < value) {
+			root.right = add(root.right, value);
+		}
+		return root;
+	}
+
+	public class IntTreeNode {
+		int data;
+		IntTreeNode left, right;
+
+		IntTreeNode(int data) {
+			this.data = data;
+			left = right = null;
+		}
+	}
+
+}
